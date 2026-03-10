c422e4d1bb240886b5d7418b68b88a759bdc1b01
Moldy-Cheese <shevaibrahim.xpro@gmail.com>
2026-03-10 15:25:34 +0700

    Tambah file TebakAngkaGame.java dari Sheva

---
+++ TebakAngkaGame.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+import java.util.Random;
+
+public class TebakAngkaGame {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        Random random = new Random();
+
+        int angkaRahasia = random.nextInt(100) + 1;
+        int tebakan = 0;
+        int jumlahPercobaan = 0;
+
+        System.out.println("=== GAME TEBAK ANGKA ===");
+        System.out.println("Tebak angka dari 1 sampai 100");
+
+        while (tebakan != angkaRahasia) {
+            System.out.print("Masukkan tebakanmu: ");
+            tebakan = input.nextInt();
+            jumlahPercobaan++;
+
+            if (tebakan > angkaRahasia) {
+                System.out.println("Terlalu besar!");
+            } 
+            else if (tebakan < angkaRahasia) {
+                System.out.println("Terlalu kecil!");
+            } 
+            else {
+                System.out.println("Selamat! Tebakanmu benar!");
+                System.out.println("Jumlah percobaan: " + jumlahPercobaan);
+            }
+        }
+
+        input.close();
+    }
+}
